534769071c2641f7b5430702809b32ce7999cb8f
Jganyo-dotcom <elikemejay@gmail.com>
2025-06-15 02:52:49 +0000

    setup

--- index.js
+++ index.js
@@ -38,22 +38,61 @@ document.querySelector('#close-modal').addEventListener('click',function(){
 })
 
 document.querySelector('#resize-height').addEventListener('input', ()=>{
+    save_current()
     document.querySelector('.design').style.height = document.querySelector('#resize-height').value + 'px'
 });
 
 document.querySelector('#resize-width').addEventListener('input', ()=>{
+    save_current()
     document.querySelector('.design').style.width = document.querySelector('#resize-width').value + 'px'
 
 })
 
 document.querySelector('#upward').addEventListener('input', ()=>{
+    save_current()
     document.querySelector('.design').style.top = document.querySelector('#upward').value + '%'
 });
 
 document.querySelector('#move-left').addEventListener('input', ()=>{
+    save_current()
     document.querySelector('.design').style.left = document.querySelector('#move-left').value + '%'
 
 })
+
+let resizeHistory=[]
+
+function save_current(){
+  resizeHistory.push({
+    width: document.querySelector('.design').style.width,
+    height: document.querySelector('.design').style.height,
+    up: document.querySelector('.design').style.top,
+    left: document.querySelector('.design').style.left
+  })  
+
+}
+document.querySelector('#undo').addEventListener('click',() =>{
+    if(resizeHistory.length===0){
+        alert('nothing to undo')
+        return;
+    };
+    let lastsize = resizeHistory.pop()
+    document.querySelector('.design').style.width = lastsize.width;
+    document.querySelector('.design').style.height= lastsize.height;
+    document.querySelector('.design').style.left = lastsize.left;
+    document.querySelector('.design').style.top = lastsize.up;
+
+    document.querySelector('#resize-width').value = parseInt(lastsize.width),
+    document.querySelector('#resize-height').value = parseInt(lastsize),
+    document.querySelector('#move-left').value = parseInt(lastsize.left)
+    document.querySelector('#upward').value = parseInt(lastsize.top)
+})
+
+function handleerror(img){
+    img.onerror = null
+    alert('poor connection')
+    img.alt= 'error';
+    img,style.display='none'
+}
 
 })
 
